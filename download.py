271f7c3773af8383ba76a58dfdb4642efa4647eb
MoroccanTea <hamza.essad@ynov.com>
2024-05-26 15:45:01 +0100

    Add forgotten pipreqs

--- download.py
+++ download.py
@@ -36,7 +36,8 @@ def main():
     
     links_file = "links.txt"
     output_folder = "downloaded_music"
-    
+
+    # Create the output folder if it doesn't exist
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
     
